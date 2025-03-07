e0b6957435c32e8e3eef0aa48d2d1d7180851578
vami <ayrane.elcapon@gmail.com>
2025-03-07 21:25:34 +0200

    cool stuff

--- src/main/java/net/vami/zoe/event/ModEventBusEvents.java
+++ src/main/java/net/vami/zoe/event/ModEventBusEvents.java
@@ -4,9 +4,14 @@ import net.minecraft.network.chat.Component;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.vami.zoe.ZoeIsntCyberpunk;
 
 @Mod.EventBusSubscriber(modid = ZoeIsntCyberpunk.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModEventBusEvents {
+
+    public static void implantRegister(FMLCommonSetupEvent event) {
+
+    }
 
 }
--- src/main/java/net/vami/zoe/item/custom/implants/ImplantItem.java
+++ src/main/java/net/vami/zoe/item/custom/implants/ImplantItem.java
@@ -1,13 +1,20 @@
 package net.vami.zoe.item.custom.implants;
 
+import net.minecraft.core.UUIDUtil;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
 
 public abstract class ImplantItem extends Item {
+    public ArrayList<AttributeModifier> attributeList = new ArrayList<>();
 
     public ImplantItem(Properties pProperties) {
         super(pProperties);
+//        ForgeRegistries.ITEMS.getKey(this);
     }
 
     public void onTick(LivingEntity entity, ItemStack item) {}
