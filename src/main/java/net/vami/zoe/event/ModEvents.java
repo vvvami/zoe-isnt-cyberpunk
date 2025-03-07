c9c119f2cfd89e758fccd6b11d4ad7bc6c438922
vami <ayrane.elcapon@gmail.com>
2025-03-07 18:25:24 +0200

    implant mod events

--- src/main/java/net/vami/zoe/event/ModEvents.java
+++ src/main/java/net/vami/zoe/event/ModEvents.java
@@ -1,21 +1,48 @@
 package net.vami.zoe.event;
 
-import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.vami.zoe.ZoeIsntCyberpunk;
-import net.vami.zoe.item.ModItems;
-import net.vami.zoe.item.custom.implants.ReinforcedTibiaItem;
+
 
 @Mod.EventBusSubscriber(modid = ZoeIsntCyberpunk.MOD_ID)
 public class ModEvents {
     @SubscribeEvent
     public static void onImplantTick(TickEvent.PlayerTickEvent event) {
         Player player = event.player;
-        if (player.getMainHandItem().is(ModItems.REINFORCED_TIBIA.get())) {
-                ModItems.REINFORCED_TIBIA.get().onTick(player, player.getMainHandItem());
-        }
+    }
+
+    @SubscribeEvent
+    public static void onImplantHit(LivingHurtEvent event) {
+        DamageSource damageSource = event.getSource();
+        Entity sourceEntity = event.getSource().getEntity();
+        Entity targetEntity = event.getEntity();
+    }
+
+    @SubscribeEvent
+    public static void onImplantHurt(LivingHurtEvent event) {
+        DamageSource damageSource = event.getSource();
+        Entity sourceEntity = event.getSource().getEntity();
+        Entity targetEntity = event.getEntity();
+    }
+
+    @SubscribeEvent
+    public static void onImplantKill(LivingDeathEvent event) {
+        DamageSource damageSource = event.getSource();
+        Entity sourceEntity = event.getSource().getEntity();
+        Entity targetEntity = event.getEntity();
+    }
+
+    @SubscribeEvent
+    public static void onImplantDeath(LivingDeathEvent event) {
+        DamageSource damageSource = event.getSource();
+        Entity sourceEntity = event.getSource().getEntity();
+        Entity targetEntity = event.getEntity();
     }
 }
